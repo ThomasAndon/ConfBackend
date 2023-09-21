6366de2ecf994858de1676c4119302fac8b2d9c0
ThomasAndon <513230863@qq.com>
2023-09-21 16:57:55 +0800

    定时删除-一分钟之前的timelog

--- server/timed.go
+++ server/timed.go
@@ -1,36 +1,37 @@
 package server
 
 import (
-	S "ConfBackend/services"
-	"ConfBackend/util"
-	"time"
+  S "ConfBackend/services"
+  "ConfBackend/util"
+  "time"
 )
 
 func StartTimed() {
 
-	// 定时更新位置
-	go StartUpdateLocationTask()
+  // 定时更新位置
+  go StartUpdateLocationTask()
 
-	go CleanPktTimeLog()
+  go CleanPktTimeLog()
 
 }
 
 // CleanPktTimeLog 清理redis中的包时间记录
 func CleanPktTimeLog() {
-	for {
+  for {
 
-		lastUpdateTimestamp := S.S.Redis.Get(S.S.Context, util.GenLatestUpdatePackageTimeKey()).Val()
+    lastUpdateTimestamp := S.S.Redis.Get(S.S.Context, util.GenLatestUpdatePackageTimeKey()).Val()
 
-		key := util.GenPacketTimelogPrefix()
+    key := util.GenPacketTimelogPrefix()
+    oneMinuteBeforeNowUnixMilli := time.Now().UnixMilli() - 60000
 
-		if lastUpdateTimestamp == "" || lastUpdateTimestamp == "0" {
-			goto SLEEP
-		}
+    if lastUpdateTimestamp == "" || lastUpdateTimestamp == "0" {
+      goto SLEEP
+    }
 
-		//根据Score删除zset，删除范围是所有算过的包都可以删除
-		S.S.Redis.ZRemRangeByScore(S.S.Context, key, "-inf", util.Float64ToString(util.StringToFloat64(lastUpdateTimestamp)-float64(1)))
-	SLEEP:
-		time.Sleep(10 * time.Second)
-	}
+    //根据Score删除zset，删除范围是所有一分钟前的数据
+    S.S.Redis.ZRemRangeByScore(S.S.Context, key, "-inf", util.Float64ToString(util.StringToFloat64(lastUpdateTimestamp)-float64(oneMinuteBeforeNowUnixMilli)))
+  SLEEP:
+    time.Sleep(10 * time.Second)
+  }
 
 }
